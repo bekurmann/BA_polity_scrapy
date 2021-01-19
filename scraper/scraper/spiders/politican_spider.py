79701c9929b23c47681723daf2e63846146a7643
Benjamin Kurmann <benjamin@kurmail.ch>
2021-01-19 22:21:48 +0100

    tryiing to get address from tel.search.ch

--- scraper/scraper/spiders/politican_spider.py
+++ scraper/scraper/spiders/politican_spider.py
@@ -15,8 +15,9 @@ class PoliticanOWSpider(scrapy.Spider):
         pagination_links = response.css('.next')
         yield from response.follow_all(pagination_links, self.parse)
 
-    def parse_politican(self, response):
 
+    def parse_politican(self, response):
+        
         def extract_with_css_name(query, position):
             # first + last name in one field -> separate!
             return response.css(query).re(r'(\w+) (\w+)')[position]
@@ -24,7 +25,14 @@ class PoliticanOWSpider(scrapy.Spider):
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
 
+        def parse_politican_address(first_name, last_name, jurisdiction):
+            address_data = scrapy.http.Response(url=f'https://tel.search.ch/api/?was={first_name}+{last_name}&wo={jurisdiction}')
+            address_data.selector.remove_namespaces()
+            return address_data.xpath("//content/text()").get()
+
+
         yield {
+            # yield extracts
             'first_name': extract_with_css_name('#nameBehoerdeMitgliedTitle b::text', 0),
             'last_name': extract_with_css_name('#nameBehoerdeMitgliedTitle b::text', 1),
             'date_of_birth': extract_with_css('#birthdayBehoerdeMitgliedContent::text'),
@@ -33,4 +41,7 @@ class PoliticanOWSpider(scrapy.Spider):
             'jurisdiction': extract_with_css('#wahlkreisPartContent span::text'),
             'fraction': extract_with_css('#fraktionenPartContent a::text'),
             
+            'street1': parse_politican_address(extract_with_css_name('#nameBehoerdeMitgliedTitle b::text', 0),
+                                                extract_with_css_name('#nameBehoerdeMitgliedTitle b::text', 1),
+                                                extract_with_css('#wahlkreisPartContent span::text')),
         }
